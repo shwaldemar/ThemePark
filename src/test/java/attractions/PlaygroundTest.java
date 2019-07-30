467c4748e7db8f921048285f60f8b738ce0f0da8
Simon <shwaldemar@gmail.com>
2019-07-30 18:02:41 +0100

    implemented interface ISecurity in playground and tobacco stall

--- src/test/java/attractions/PlaygroundTest.java
+++ src/test/java/attractions/PlaygroundTest.java
@@ -2,15 +2,19 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class PlaygroundTest {
     Playground playground;
+    Visitor visitor1, visitor2;
 
     @Before
     public void setUp() throws Exception {
         playground = new Playground("Fun Zone", 7);
+        visitor1 = new Visitor(12, 6.20, 20.00);
+        visitor2 = new Visitor(16, 6.20, 20.00);
     }
 
     @Test
@@ -26,5 +30,16 @@ public class PlaygroundTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, playground.getVisitCount());
+    }
+
+    @Test
+    public void willAllowValidVisitor() {
+        assertEquals(true, playground.isAllowedTo(visitor1));
+
+    }
+
+    @Test
+    public void willDenyInValidVisitor() {
+        assertEquals(false, playground.isAllowedTo(visitor2));
     }
 }
--- src/test/java/stalls/TobaccoStallTest.java
+++ src/test/java/stalls/TobaccoStallTest.java
@@ -2,16 +2,21 @@ package stalls;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class TobaccoStallTest {
 
     TobaccoStall tobaccoStall;
+    Visitor visitor1, visitor2;
 
     @Before
     public void setUp() throws Exception {
         tobaccoStall = new TobaccoStall("Jacks Drum", "Jack Jarvis", ParkingSpot.B1);
+        visitor1 = new Visitor(18, 6.20, 20.00);
+        visitor2 = new Visitor(16, 6.20, 20.00);
+
     }
 
     @Test
@@ -27,5 +32,16 @@ public class TobaccoStallTest {
     @Test
     public void hasParkingSpot() {
         assertEquals(ParkingSpot.B1, tobaccoStall.getParkingSpot());
+    }
+
+    @Test
+    public void willAllowValidVisitor() {
+        assertEquals(true, tobaccoStall.isAllowedTo(visitor1));
+
+    }
+
+    @Test
+    public void willDenyInValidVisitor() {
+        assertEquals(false, tobaccoStall.isAllowedTo(visitor2));
     }
 }
