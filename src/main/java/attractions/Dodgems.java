beac2003b43b61d442861c929f3830a447fa5dda
Simon <shwaldemar@gmail.com>
2019-07-31 10:00:16 +0100

    added price functionality to dodgems

--- src/main/java/attractions/Dodgems.java
+++ src/main/java/attractions/Dodgems.java
@@ -1,10 +1,27 @@
 package attractions;
 
 import behaviours.IReviewed;
+import behaviours.ITicketed;
+import people.Visitor;
 
-public class Dodgems extends Attraction implements IReviewed {
+public class Dodgems extends Attraction implements IReviewed, ITicketed {
 
-    public Dodgems(String name, int rating) {
+    public Dodgems(String name, int rating, double price) {
+
         super(name, rating);
     }
+
+    public double defaultPrice(Visitor visitor) {
+        if (visitor.getAge() <= 12) {
+            return 4.50 / 2;
+        }
+        else {
+            return 4.50;
+        }
+    }
+
+    public double priceForVisitor(Visitor visitor) {
+        return 4.50;
+    }
+
 }
--- src/test/java/attractions/DodgemTest.java
+++ src/test/java/attractions/DodgemTest.java
@@ -2,16 +2,22 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class DodgemTest {
 
     Dodgems dodgems;
+    Visitor visitor1, visitor2;
 
     @Before
     public void setUp() throws Exception {
-        dodgems = new Dodgems("Bumper Cars", 5);
+        dodgems = new Dodgems("Bumper Cars", 5, 4.50);
+        visitor1 = new Visitor(14, 210, 20.00);
+        visitor2 = new Visitor(11, 124, 20.00);
+
+
     }
 
 
@@ -28,5 +34,15 @@ public class DodgemTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, dodgems.getVisitCount());
+    }
+
+    @Test
+    public void canChargeFullPriceForAge12Plus() {
+        assertEquals(4.50, dodgems.defaultPrice(visitor1), 0.01);
+    }
+
+    @Test
+    public void canChargeNormalForAgeLessThan12() {
+        assertEquals(2.25, dodgems.defaultPrice(visitor2), 0.01);
     }
 }
